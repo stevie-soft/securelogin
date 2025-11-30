2ac943f1f403cf79a33cf2c7862135640235da20
stevie-soft <108979001+stevie-soft@users.noreply.github.com>
2025-12-01 00:02:34 +0100

    public | index.php | fix missing controller for root path

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@ require_once '../src/bootstrap.php';
 $path = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 $method = $_SERVER['REQUEST_METHOD'];
 $router = new Router([
+  '' => HomeController::class,
   'login' => LoginController::class,
   'profile' => ProfileController::class,
 ]);
