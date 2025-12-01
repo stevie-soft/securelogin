84f4ac80991246cb42e716ba1fd25d01566cb657
stevie-soft <108979001+stevie-soft@users.noreply.github.com>
2025-12-01 04:00:18 +0100

    login | bad password logic | add countdown before redirect

--- public/js/login.js
+++ public/js/login.js
@@ -20,15 +20,61 @@ function getErrorCode() {
   return errorCode;
 }
 
+class ErrorBox {
+  constructor() {
+    this.container = document.getElementById("errorBox");
+  }
+
+  setText(text) {
+    this.container.textContent = text;
+  }
+}
+
+async function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
+
+class CountdownBox {
+  constructor(seconds, url) {
+    this.url = url;
+    this.container = document.getElementById("countdownBox");
+    this.counter = seconds;
+  }
+
+  async start() {
+    this.refresh();
+
+    for (let i = 0; i < this.counter + 2; i++) {
+      await sleep(1000);
+
+      this.decrease();
+    }
+
+    window.location.replace(this.url);
+  }
+
+  decrease() {
+    this.counter--;
+    this.refresh();
+
+    if (this.counter === 0) {
+      window.location.replace(this.url);
+    }
+  }
+
+  refresh() {
+    this.container.textContent = "Átirányítás " + this.counter + "...";
+  }
+}
+
 function displayError(errorCode) {
-  const errorBox = document.getElementById("errorBox");
-  errorBox.textContent = getErrorMessage(errorCode);
+  const errorBox = new ErrorBox();
+  errorBox.setText(getErrorMessage(errorCode));
 }
 
 async function triggerPolice() {
-  setTimeout(() => {
-    window.location.replace("https://police.hu");
-  }, 3000);
+  const countdownBox = new CountdownBox(3, "https://police.hu");
+  countdownBox.start();
 }
 
 function handleError() {
